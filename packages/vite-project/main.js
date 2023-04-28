8dc057055f928cc2374b1c3d21baca0d12807dcb
Jinjiang <zhaojinjiang@me.com>
2023-04-28 16:33:55 +0800

    reproduce the bug

--- packages/vite-project/main.js
+++ packages/vite-project/main.js
@@ -1,24 +1,7 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// // this would work
+// import eIndexOf from 'es5-ext/array/#/e-index-of.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// this would fail, which ate # and resulted in: /.../es5-ext/array/?import&v=e83a80a5
+import eIndexOf from '../foo/node_modules/es5-ext/array/#/e-index-of.js'
 
-setupCounter(document.querySelector('#counter'))
+console.log(eIndexOf)
