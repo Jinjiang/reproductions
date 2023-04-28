2df72b2bc44c33967641870f589c2c0f1df80f55
Jinjiang <zhaojinjiang@me.com>
2023-04-28 12:00:01 +0800

    reproduce the failure

--- packages/vite-project/main.js
+++ packages/vite-project/main.js
@@ -1,2 +1,10 @@
-import { foo } from 'foo'
+// // this would work
+// import { foo } from './foo'
+
+// // this would work
+// import { foo } from '../foo'
+
+// this would fail
+import { foo } from '../bar/node_modules/foo'
+
 console.log(foo())
