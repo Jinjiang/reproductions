be6f9e432b50afc8b2436628d1049cfe47432b87
Jinjiang <zhaojinjiang@me.com>
2023-05-02 09:40:12 +0800

    add another case about regexpu-core

--- packages/vite-project/main.js
+++ packages/vite-project/main.js
@@ -1,10 +1,22 @@
+// CASE 1:
+
 // // this would work
 // import { foo } from './foo'
 
 // // this would work
 // import { foo } from '../foo'
 
-// this would fail
+// // this would fail
 import { foo } from '../bar/node_modules/foo'
 
 console.log(foo())
+
+// CASE 2:
+
+// // this would work
+// import rewritePattern from 'regexpu-core'
+
+// this would fail
+import rewritePattern from '../bar/node_modules/regexpu-core'
+
+console.log(rewritePattern('foo.bar', 'u'))
