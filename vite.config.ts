5a4f12ea7c0cd5721aea167c3e7822f4b827058c
Jinjiang <zhaojinjiang@me.com>
2023-06-15 15:59:31 +0800

    add debug files

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import nodePolyfills from 'vite-plugin-node-stdlib-browser';
+import react from '@vitejs/plugin-react';
+import mdx from 'vite-plugin-mdx';
+
+export default {
+  resolve: {
+    alias: [
+      {
+        // this is required for the SCSS modules
+        find: /^~(.*)$/,
+        replacement: '$1'
+      }
+    ],
+  },
+  plugins: [
+    (nodePolyfills as any)(),
+    react(),
+    mdx(),
+  ],
+};
