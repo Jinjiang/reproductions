2aece9c8246df6eec12b120e311ff40e1bfc74e8
Jinjiang <zhaojinjiang@me.com>
2023-06-15 16:19:31 +0800

    collect debug entry file

--- entry.js
+++ entry.js
@@ -1,1 +1,103 @@
+
+// // overview
+
+// import { linkModules } from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\preview\\dist\\preview.preview.runtime.js';
+
+// import * as file_0_0 from "C:\\Users\\zhaoj\\code\\debug-vite-dev-server-2\\node_modules\\.bit_roots\\teambit.vue_vue@0.1.17\\node_modules\\@teambit\\vue.examples.hello-world\\dist\\hello-world.docs.md";
+
+// import * as defaultMainModule from "C:\\Users\\zhaoj\\code\\debug-vite-dev-server-2\\node_modules\\.pnpm\\@teambit+vue.vue@0.1.17_@teambit+legacy@node_modules+@teambit+legacy\\node_modules\\@teambit\\vue.vue\\dist\\preview\\docs.js";
+// import * as teambitVueVue0117MainModule from "C:\\Users\\zhaoj\\code\\debug-vite-dev-server-2\\node_modules\\.pnpm\\@teambit+vue.vue@0.1.17_@teambit+legacy@node_modules+@teambit+legacy\\node_modules\\@teambit\\vue.vue\\dist\\preview\\docs.js";
+
+// linkModules('overview', {
+//   modulesMap: {
+//     "default": defaultMainModule,
+//     "teambit.vue/vue@0.1.17": teambitVueVue0117MainModule
+//   },
+//   isSplitComponentBundle: false,
+//   componentMap: {
+//     "examples/hello-world": [file_0_0]
+//   }
+// });
+
+
+// // compositions
+
+// import { linkModules } from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\preview\\dist\\preview.preview.runtime.js';
+
+// import * as file_0_0 from "C:\\Users\\zhaoj\\code\\debug-vite-dev-server-2\\node_modules\\.bit_roots\\teambit.vue_vue@0.1.17\\node_modules\\@teambit\\vue.examples.hello-world\\dist\\hello-world.composition.js";
+
+// import * as defaultMainModule from "C:\\Users\\zhaoj\\code\\debug-vite-dev-server-2\\node_modules\\.pnpm\\@teambit+vue.vue@0.1.17_@teambit+legacy@node_modules+@teambit+legacy\\node_modules\\@teambit\\vue.vue\\dist\\preview\\mounter.js";
+// import * as teambitVueVue0117MainModule from "C:\\Users\\zhaoj\\code\\debug-vite-dev-server-2\\node_modules\\.pnpm\\@teambit+vue.vue@0.1.17_@teambit+legacy@node_modules+@teambit+legacy\\node_modules\\@teambit\\vue.vue\\dist\\preview\\mounter.js";
+
+// linkModules('compositions', {
+//   modulesMap: {
+//     "default": defaultMainModule,
+//     "teambit.vue/vue@0.1.17": teambitVueVue0117MainModule
+//   },
+//   isSplitComponentBundle: false,
+//   componentMap: {
+//     "examples/hello-world": [file_0_0]
+//   }
+// });
+
+
+// // preview.root
+
+// import { Harmony } from '@teambit/harmony';
+// import previewAspect from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\preview\\dist\\preview.aspect.js';
+// import docsAspect from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\docs\\dist\\docs.aspect.js';
+// import compositionsAspect from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\compositions\\dist\\compositions.aspect.js';
+// import pubsubAspect from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\pubsub\\dist\\pubsub.aspect.js';
+// import reactAspect from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\react\\dist\\react.aspect.js';
+// import reactNativeAspect from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\react-native\\dist\\react-native.aspect.js';
+// import commandBarAspect from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\command-bar\\dist\\command-bar.aspect.js';
+// import previewRuntime from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\preview\\dist\\preview.preview.runtime.js';
+// import docsRuntime from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\docs\\dist\\docs.preview.runtime.js';
+// import compositionsRuntime from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\compositions\\dist\\compositions.preview.runtime.js';
+// import pubsubRuntime from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\pubsub\\dist\\pubsub.preview.runtime.js';
+// import reactRuntime from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\react\\dist\\react.preview.runtime.js';
+// import reactNativeRuntime from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\react-native\\dist\\react-native.preview.runtime.js';
+// import commandBarRuntime from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\command-bar\\dist\\command-bar.preview.runtime.js';
+
+// const isBrowser = typeof window !== "undefined";
+// const config = JSON.parse('{"teambit.workspace/workspace":{"name":"my-workspace-name","icon":"https://static.bit.dev/bit-logo.svg","defaultDirectory":"{scope}/{name}","defaultScope":"my-scope"},"teambit.dependencies/dependency-resolver":{"packageManager":"teambit.dependencies/pnpm","policy":{"dependencies":{"@mdx-js/react":"^1.6.22","@vitejs/plugin-react":"^4.0.0","node-stdlib-browser":"^1.2.0","react":"^17","react-dom":"^17","vite":"4.1.0","vite-plugin-mdx":"^3.5.11","vite-plugin-node-polyfills":"^0.9.0","vite-plugin-node-stdlib-browser":"^0.2.1"},"peerDependencies":{}},"linkCoreAspects":true,"rootComponents":true},"teambit.workspace/variants":{"*":{}},"teambit.harmony/bit":"teambit.workspace/workspace"}');
+// previewAspect.id = 'teambit.preview/preview';
+// docsAspect.id = 'teambit.docs/docs';
+// compositionsAspect.id = 'teambit.compositions/compositions';
+// pubsubAspect.id = 'teambit.harmony/pubsub';
+// reactAspect.id = 'teambit.react/react';
+// reactNativeAspect.id = 'teambit.react/react-native';
+// commandBarAspect.id = 'teambit.explorer/command-bar';
+// export default function render(...props){
+//   return Harmony.load([previewAspect, docsAspect, compositionsAspect, pubsubAspect, reactAspect, reactNativeAspect, commandBarAspect], 'preview', config)
+//     .then((harmony) => {
+//       return harmony
+//       .run()
+//       .then(() => harmony.get('teambit.preview/preview'))
+//       .then((rootExtension) => {
+//         const ssrSetup = !isBrowser && rootExtension.setupSsr;
+//         const setup = rootExtension.setup;
+//         const setupFunc = (ssrSetup || setup || function noop(){}).bind(rootExtension);
+
+//         return (
+//           Promise.resolve(setupFunc())
+//             .then(() => rootExtension)
+//         );
+//       })
+//       .then((rootExtension) => {
+//         if (isBrowser) {
+//           return rootExtension.render('teambit.workspace/workspace', ...props);
+//         } else {
+//           return rootExtension.renderSsr('teambit.workspace/workspace', ...props);
+//         }
+//       })
+//       .catch((err) => {
+//         throw err;
+//       });
+//     });
+// }
+
+// if (isBrowser || 'preview' === 'main') render();
+
+
 console.log('hello world')
