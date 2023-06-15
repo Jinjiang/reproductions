24032cc424258740927ee1005a07c438fefb74d1
Jinjiang <zhaojinjiang@me.com>
2023-06-15 16:45:39 +0800

    update debug file

--- entry.js
+++ entry.js
@@ -1,5 +1,5 @@
 
-// // overview
+/* overview entry file */
 
 // import { linkModules } from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\preview\\dist\\preview.preview.runtime.js';
 
@@ -20,7 +20,7 @@
 // });
 
 
-// // compositions
+/* compositions entry file */
 
 // import { linkModules } from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\preview\\dist\\preview.preview.runtime.js';
 
@@ -41,7 +41,7 @@
 // });
 
 
-// // preview.root
+/* ui/dist/preview.root entry file */
 
 // import { Harmony } from '@teambit/harmony';
 // import previewAspect from 'C:\\Users\\zhaoj\\AppData\\Local\\.bvm\\versions\\0.1.60\\bit-0.1.60\\node_modules\\@teambit\\preview\\dist\\preview.aspect.js';
@@ -98,6 +98,167 @@
 // }
 
 // if (isBrowser || 'preview' === 'main') render();
+
+
+/* debugging code */
+
+import { linkModules } from '@teambit/preview/dist/preview.preview.runtime.js';
+
+import * as file_0_0 from "@teambit/vue.examples.hello-world/dist/hello-world.docs.md";
+// // TODO: NOT WORKING
+// import * as defaultMainModule from "@teambit/vue.vue/dist/preview/docs.js";
+// import * as teambitVueVue0117MainModule from "@teambit/vue.vue/dist/preview/docs.js";
+
+import * as file_0_0_2 from "@teambit/vue.examples.hello-world/dist/hello-world.composition.js";
+import * as defaultMainModule_2 from "@teambit/vue.vue/dist/preview/mounter.js";
+import * as teambitVueVue0117MainModule_2 from "@teambit/vue.vue/dist/preview/mounter.js";
+
+import { Harmony } from '@teambit/harmony';
+import previewAspect from '@teambit/preview/dist/preview.aspect.js';
+import docsAspect from '@teambit/docs/dist/docs.aspect.js';
+import compositionsAspect from '@teambit/compositions/dist/compositions.aspect.js';
+import pubsubAspect from '@teambit/pubsub/dist/pubsub.aspect.js';
+import reactAspect from '@teambit/react/dist/react.aspect.js';
+import reactNativeAspect from '@teambit/react-native/dist/react-native.aspect.js';
+import commandBarAspect from '@teambit/command-bar/dist/command-bar.aspect.js';
+import previewRuntime from '@teambit/preview/dist/preview.preview.runtime.js';
+import docsRuntime from '@teambit/docs/dist/docs.preview.runtime.js';
+import compositionsRuntime from '@teambit/compositions/dist/compositions.preview.runtime.js';
+import pubsubRuntime from '@teambit/pubsub/dist/pubsub.preview.runtime.js';
+import reactRuntime from '@teambit/react/dist/react.preview.runtime.js';
+import reactNativeRuntime from '@teambit/react-native/dist/react-native.preview.runtime.js';
+import commandBarRuntime from '@teambit/command-bar/dist/command-bar.preview.runtime.js';
+
+// // TODO: NOT WORKING
+// linkModules('overview', {
+//   modulesMap: {
+//     "default": defaultMainModule,
+//     "teambit.vue/vue@0.1.17": teambitVueVue0117MainModule
+//   },
+//   isSplitComponentBundle: false,
+//   componentMap: {
+//     "examples/hello-world": [file_0_0]
+//   }
+// });
+
+linkModules('compositions', {
+  modulesMap: {
+    "default": defaultMainModule_2,
+    "teambit.vue/vue@0.1.17": teambitVueVue0117MainModule_2
+  },
+  isSplitComponentBundle: false,
+  componentMap: {
+    "examples/hello-world": [file_0_0_2]
+  }
+});
+
+const isBrowser = typeof window !== "undefined";
+
+const config = {
+  "teambit.workspace/workspace": {
+    "name": "my-workspace-name",
+    "icon": "https://static.bit.dev/bit-logo.svg",
+    "defaultDirectory": "{scope}/{name}",
+    "defaultScope": "my-scope"
+  },
+  "teambit.dependencies/dependency-resolver": {
+    "packageManager": "teambit.dependencies/pnpm",
+    "policy": {
+      "dependencies": {
+        "@mdx-js/react": "^1.6.22",
+        "@vitejs/plugin-react": "^4.0.0",
+        "node-stdlib-browser": "^1.2.0",
+        "react": "^17",
+        "react-dom": "^17",
+        "vite": "4.1.0",
+        "vite-plugin-mdx": "^3.5.11",
+        "vite-plugin-node-polyfills": "^0.9.0",
+        "vite-plugin-node-stdlib-browser": "^0.2.1"
+      },
+      "peerDependencies": {}
+    },
+    "linkCoreAspects": true,
+    "rootComponents": true
+  },
+  "teambit.workspace/variants": {
+    "*": {}
+  },
+  "teambit.harmony/bit": "teambit.workspace/workspace"
+};
+
+previewAspect.id = 'teambit.preview/preview';
+docsAspect.id = 'teambit.docs/docs';
+compositionsAspect.id = 'teambit.compositions/compositions';
+pubsubAspect.id = 'teambit.harmony/pubsub';
+reactAspect.id = 'teambit.react/react';
+reactNativeAspect.id = 'teambit.react/react-native';
+commandBarAspect.id = 'teambit.explorer/command-bar';
+function render(...props){
+  return Harmony.load([
+    previewAspect,
+    docsAspect,
+    compositionsAspect,
+    pubsubAspect,
+    reactAspect,
+    reactNativeAspect,
+    commandBarAspect
+  ], 'preview', config)
+    .then((harmony) => {
+      return harmony
+      .run()
+      .then(() => harmony.get('teambit.preview/preview'))
+      .then((rootExtension) => {
+        const ssrSetup = !isBrowser && rootExtension.setupSsr;
+        const setup = rootExtension.setup;
+        const setupFunc = (ssrSetup || setup || function noop(){}).bind(rootExtension);
+
+        return (
+          Promise.resolve(setupFunc())
+            .then(() => rootExtension)
+        );
+      })
+      .then((rootExtension) => {
+        if (isBrowser) {
+          return rootExtension.render('teambit.workspace/workspace', ...props);
+        } else {
+          return rootExtension.renderSsr('teambit.workspace/workspace', ...props);
+        }
+      })
+      .catch((err) => {
+        throw err;
+      });
+    });
+}
+
+// // TODO: NOT WORKING
+// if (isBrowser || 'preview' === 'main') render();
+
+
+/* log information */
+
+console.log({
+  linkModules,
+
+  file_0_0,
+  // defaultMainModule,
+  // teambitVueVue0117MainModule,
+
+  file_0_0_2,
+  defaultMainModule_2,
+  teambitVueVue0117MainModule_2,
+
+  previewRuntime,
+  docsRuntime,
+  compositionsRuntime,
+  pubsubRuntime,
+  reactRuntime,
+  reactNativeRuntime,
+  commandBarRuntime,
+
+  isBrowser,
+  config,
+  render,
+})
 
 
 console.log('hello world')
