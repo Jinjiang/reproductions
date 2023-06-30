5f5a49e34327d834798b006dcbb49aecdd6c17cb
Jinjiang <zhaojinjiang@me.com>
2023-06-30 14:18:29 +0800

    reproduce cjs in node_modules not as a package

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ How to reproduce:
 ```bash
 pnpm install
 pnpm dev
+# see comments in `main.js`
 ```
---
+++ main.js
@@ -0,0 +1,2 @@
+// // to reproduce: run `pnpm dev` then uncomment this line and save
+// import './node_modules/.cache/foo.js'
