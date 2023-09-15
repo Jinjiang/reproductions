e7e441cf2e6686641893a5dd279e3532ba4436ef
Jinjiang <zhaojinjiang@me.com>
2023-09-15 14:24:29 +0800

    make it works

--- README.md
+++ README.md
@@ -4,9 +4,3 @@ How to reproduce:
 pnpm install
 pnpm dev
 ```
-
-error message:
-
-```
-Uncaught SyntaxError: The requested module '/@fs/Users/xxx/reproductions/node_modules/.pnpm/react@18.2.0/node_modules/react/jsx-runtime.js?v=7e9f9773' does not provide an export named 'Fragment' (at app.js?v=7e9f9773:1:38)
-```
--- run.js
+++ run.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 const config = defineConfig({
   // // This works
   // root: './',
-  // This doesn't
-  root: './node_modules/.bit_roots/foo',
+  // // This doesn't
+  // root: './node_modules/.bit_roots/foo',
+  // This works
+  root: './.bit_roots/foo',
   plugins: [react()],
 });
 
