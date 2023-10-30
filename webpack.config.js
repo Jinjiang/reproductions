37755723b130a3f72b1bb0a40c26a8bd159348d4
Jinjiang <zhaojinjiang@me.com>
2023-10-30 14:16:51 +0800

    simplify the setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,8 @@ const path = require('path');
 const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const isDevelopment = process.env.NODE_ENV !== 'production';
-
 module.exports = {
-  mode: isDevelopment ? 'development' : 'production',
+  mode: 'development',
   devServer: {
     client: { overlay: false },
   },
@@ -16,13 +14,15 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        include: path.join(__dirname, 'src'),
+        include: [
+          path.join(__dirname, 'src'),
+        ],
         use: 'babel-loader',
       },
     ],
   },
   plugins: [
-    isDevelopment && new ReactRefreshPlugin(),
+    new ReactRefreshPlugin(),
     new HtmlWebpackPlugin({
       filename: './index.html',
       template: './public/index.html',
