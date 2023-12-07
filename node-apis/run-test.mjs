de4d830a95e8b756ce8640549095fac03440276e
Jinjiang <zhaojinjiang@me.com>
2023-12-07 14:16:30 +0800

    reproduce

--- node-apis/run-test.mjs
+++ node-apis/run-test.mjs
@@ -2,13 +2,27 @@ import { dirname, resolve } from 'path'
 import { fileURLToPath } from 'url';
 
 import { startVitest } from 'vitest/node'
-import vue from '@vitejs/plugin-vue'
+// import vue from '@vitejs/plugin-vue'
+
+import react from '@vitejs/plugin-react';
+import mdx from '@mdx-js/rollup';
 
 import MyReporter from './reporter.mjs'
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const myReporter = new MyReporter()
+
+
+// import { defineConfig } from 'vite';
+// export default defineConfig({
+//   plugins: [react(), mdx()],
+//   test: {
+//     cache: {
+//       dir: '.vitest'
+//     }
+//   }
+// });
 
 const vitest = await startVitest(
   'test',
@@ -31,15 +45,31 @@ const vitest = await startVitest(
   // },
 
   {
+    root: '/Volumes/jinjiang-portable-disk/Developer/teambit/temp/debug-beer-shop',
+    configFile: false,
+    envFile: false,
+    server: { fs: { strict: false } },
     plugins: [
-      vue(),
+      // vue(),
+      react(), mdx(),
     ],
     test: {
-      root: resolve(__dirname, '../src'),
+      // root: resolve(__dirname, '../src'),
+      root: '/Volumes/jinjiang-portable-disk/Developer/teambit/temp/debug-beer-shop',
       config: false,
       globals: true,
       environment: 'jsdom',
       watch: false,
+      exclude: [
+        '**/node_modules/**',
+        '**/cypress/**',
+        '**/.{idea,git,cache,output,temp}/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*'
+      ],
+      include: [
+        '/Volumes/jinjiang-portable-disk/Developer/teambit/temp/debug-beer-shop/beer-shop/blocks/top-beers/**/*.spec.*',
+        '/Volumes/jinjiang-portable-disk/Developer/teambit/temp/debug-beer-shop/beer-shop/blocks/top-beers/**/*.test.*'
+      ],
       reporters: [myReporter],
     },
   }
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import mdx from '@mdx-js/rollup';
+
+export default defineConfig({
+  root: '/Volumes/jinjiang-portable-disk/Developer/teambit/temp/debug-beer-shop',
+  server: { fs: { strict: false } },
+  plugins: [
+    react(), mdx(),
+  ],
+  test: {
+    root: '/Volumes/jinjiang-portable-disk/Developer/teambit/temp/debug-beer-shop',
+    globals: true,
+    environment: 'jsdom',
+    watch: false,
+    exclude: [
+      '**/node_modules/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*'
+    ],
+    include: [
+      '/Volumes/jinjiang-portable-disk/Developer/teambit/temp/debug-beer-shop/beer-shop/blocks/top-beers/**/*.spec.*',
+      '/Volumes/jinjiang-portable-disk/Developer/teambit/temp/debug-beer-shop/beer-shop/blocks/top-beers/**/*.test.*'
+    ],
+  },
+})
