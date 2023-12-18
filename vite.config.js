2b43ac56365ba91fefc4f0ac35311223ba4091dd
Jinjiang <zhaojinjiang@me.com>
2023-12-18 23:38:56 +0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,24 @@
+const modules = ['foo-cjs', 'foo-esm']
+
 module.exports = {
+  // doesn't work as expected
+  server: {
+    watch: {
+      ignored: modules.map((m) => `!**/node_modules/${m}/**`)
+    }
+  },
+  // doesn't work as expected
+  optimizeDeps: {
+    exclude: modules
+  },
   test: {
-    globals: true
+    globals: true,
+    server: {
+      deps: {
+        inline: modules
+      }
+    },
+    // doesn't work as expected
+    watchExclude: modules.map((m) => `!**/node_modules/${m}/**`)
   }
 }
