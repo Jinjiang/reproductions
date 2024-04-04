d25cf06dbb5c7501f4f65f30648e20dff2172083
Jinjiang <zhaojinjiang@me.com>
2024-04-04 11:16:26 +0800

    make foo mutable

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const packageList = ["foo"];
+
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    watch: {
+      ignored: [...packageList.map(p => `!**/node_modules/${p}/**`),]
+    }
+  },
+  optimizeDeps: {
+    exclude: packageList,
+  },
+  plugins: [react({
+    include: [/node_modules\/foo/],
+  })],
 });
