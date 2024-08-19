bf18383cad2a45f02ddc17fdcb4f1c2070e30db4
Jinjiang <zhaojinjiang@me.com>
2024-08-19 11:42:38 +0800

    reproduce

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ How to reproduce:
 pnpm install
 pnpm dev
 ```
+
+index.html: src="c:\xxx\.git\a.js"
+  a.js -> import 'c:\xxx\node_modules\{preview\preview.js,local\local.mjs,x\x.md}'
+    local.js -> import 'c:\xxx\node_modules\docs\docs.js'
+watch packages: x
---
+++ main.mjs
@@ -0,0 +1,5 @@
+import preview from 'C:\\Users\\zhaoj\\code\\reproductions\\node_modules\\preview\\preview.js';
+import local from 'C:\\Users\\zhaoj\\code\\reproductions\\node_modules\\local\\local.js';
+import x from 'C:\\Users\\zhaoj\\code\\reproductions\\node_modules\\prexview\\x.vue';
+
+console.log('main.mjs', { preview, local, x });
