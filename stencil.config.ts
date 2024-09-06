7d24bd4cbeaf630017a93cb55671e51f6f0b6719
Jinjiang <zhaojinjiang@me.com>
2024-09-06 15:59:23 +0800

    simplify config

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,22 +3,23 @@ import { Config } from '@stencil/core';
 export const config: Config = {
   namespace: 'try-stencil',
   outputTargets: [
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
-      type: 'dist-custom-elements',
-      customElementsExportBehavior: 'auto-define-custom-elements',
-      externalRuntime: false,
-    },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-    },
+    { type: 'dist' },
+    // {
+    //   type: 'dist',
+    //   esmLoaderPath: '../loader',
+    // },
+    // {
+    //   type: 'dist-custom-elements',
+    //   customElementsExportBehavior: 'auto-define-custom-elements',
+    //   externalRuntime: false,
+    // },
+    // {
+    //   type: 'docs-readme',
+    // },
+    // {
+    //   type: 'www',
+    //   serviceWorker: null, // disable service workers
+    // },
   ],
   testing: {
     browserHeadless: "new",
