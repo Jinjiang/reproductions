09ce6aa48746dd9aaa5deee8fdffb5b1b72858cf
Jinjiang <zhaojinjiang@me.com>
2024-09-16 11:25:57 +0800

    use forked react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { createRequire } from 'module'
 
 const require = createRequire(import.meta.url);
 
 const packages = ['foo']
+
+const { default: react } = await import('@teambit/vite.plugins.vite-plugin-react')
+
+const generateExcludeRegExp = (packages: string[]): RegExp => {
+  const packageRegExp = packages.join('|');
+  return new RegExp(`node_modules(?!.*node_modules/(${packageRegExp}))(?!.*/(${packageRegExp}))`);
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,5 +23,9 @@ export default defineConfig({
     entries: packages.map((p) => require.resolve(p)),
     exclude: packages
   },
-  plugins: [react()],
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  plugins: [react.default({
+    exclude: [generateExcludeRegExp(packages)]
+  })],
 })
