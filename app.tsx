ddd938cc131030a1f68dfa75da9cdf757236a44d
Jinjiang <zhaojinjiang@me.com>
2024-10-01 08:47:46 +0800

    mdx setup

--- app.tsx
+++ app.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
 
+import { ThemeSwitcher } from '@teambit/design.themes.theme-toggler';
+import { MDXLayout } from '@teambit/mdx.ui.mdx-layout';
+
+import Content from './docs.mdx';
+
+export type DocsAppProps = {
+  children: React.ReactChild;
+};
+
+export function DocsTheme({ children }: DocsAppProps) {
+  return (
+    <ThemeSwitcher defaultTheme={'light'}>
+      {children}
+    </ThemeSwitcher>
+  );
+}
+
 export function App() {
-  return <div>hello world</div>;
+  return (
+    <ThemeSwitcher defaultTheme={'light'}>
+      <MDXLayout>
+        <div>hello world</div>
+        <Content />
+      </MDXLayout>
+    </ThemeSwitcher>
+  );
 }
