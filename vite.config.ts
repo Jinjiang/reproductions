6935aa72349c42fd0b2dc28a6b4b5a53af8e080c
Jinjiang <zhaojinjiang@me.com>
2024-10-04 12:55:27 +0800

    replace pnpm.override with alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+import { createRequire } from "module";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import mdx from "@mdx-js/rollup";
 
 import { mdxPrePlugin } from "./config/mdx-pre-plugin";
 import { mdxOptions } from "./config/mdx-options";
+
+const require = createRequire(import.meta.url);
+const mdxJsReactPath = require.resolve("@mdx-js/react");
+console.log({ mdxJsReactPath });
 
 export default defineConfig({
   css: {
@@ -12,6 +17,14 @@ export default defineConfig({
         api: "modern-compiler"
       }
     },
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@mdx-js/react',
+        replacement: mdxJsReactPath,
+      },
+    ],
   },
   plugins: [
     react(),
