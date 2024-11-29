8dfd0be008f9638fbfdbe238ac159c721c603c4c
Jinjiang <zhaojinjiang@me.com>
2024-11-29 17:13:10 +0800

    setup

---
+++ main.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from '@bitdev/react.examples.button';
+
+console.log({
+  React,
+  ReactDOM,
+  Button,
+});
---
+++ vite.config.ts
@@ -0,0 +1,50 @@
+import { createRequire } from 'module';
+import { defineConfig } from "vite";
+import { babel } from '@rollup/plugin-babel';
+import findRoot from 'find-root';
+
+const esmRequire = createRequire(import.meta.url);
+
+const highlighterPath = esmRequire.resolve('@teambit/react.babel.bit-react-transformer');
+console.log({ highlighterPath });
+
+export default defineConfig({
+  build: {
+    minify: false,
+  },
+  plugins: [
+    babel({
+      filter: (id: string | unknown) => {
+        if (typeof id !== 'string') {
+          console.log('unknown', id);
+          return false;
+        }
+        console.log('filter', id);
+        if (
+          id.match(/^\//) &&
+          id.match(/\.js$/) &&
+          id.match(/node_modules/)
+        ) {
+          try {
+            const root = findRoot(id);
+            console.log({ root });
+            console.log(`${root}/package.json`);
+            const packageJson = esmRequire(`${root}/package.json`);
+            if (packageJson.componentId) {
+              console.log('filtered', packageJson.componentId);
+              console.log(id);
+              return true;
+            }
+          } catch (e) {
+            // ignore error
+          }
+        }
+        return false;
+      },
+      babelHelpers: 'bundled',
+      plugins: [
+        highlighterPath,
+      ],
+    }),
+  ]
+});
