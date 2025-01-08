8687ba98c7be3b3279a0a0198b4c0d95dbe56d33
Jinjiang <zhaojinjiang@me.com>
2025-01-08 12:42:08 +0800

    setup typescript and more rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,54 @@
-import js from "@eslint/js";
+import globals from "globals"
+import jsLint from "@eslint/js"
+import tsLint from "typescript-eslint"
+// import vueLint from "eslint-plugin-vue"
 
 export default [
-    js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,jsx,tsx}"],
+    languageOptions: {
+      // common parser options, enable TypeScript and JSX
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        sourceType: "module"
+      },
+    },
+  },
 
-   {
-       rules: {
-           "no-unused-vars": "warn",
-           "no-undef": "warn"
-       }
-   }
-];
+  // {
+  //   files: ["*.vue", "**/*.vue"],
+  //   languageOptions: {
+  //     parser: "vue-eslint-parser",
+  //     parserOptions: {
+  //       // <script lang="ts" /> to enable TypeScript in Vue SFC
+  //       parser: "@typescript-eslint/parser",
+  //       sourceType: "module"
+  //     }
+  //   }
+  // },
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+    },
+  },
+
+  // syntax rules
+  jsLint.configs.recommended,
+  ...tsLint.configs.recommended,
+  // ...vueLint.configs["flat/essential"],
+
+  // {
+  //   ignores: ["node_modules", "dist", "build", "**/node_modules"],
+  // },
+
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn"
+    }
+  }
+]
