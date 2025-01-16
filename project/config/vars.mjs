ba2cd7b5c28a6a3d489ad1014159dc812812e12f
Jinjiang <zhaojinjiang@me.com>
2025-01-16 16:20:16 +0800

    move config into project

--- project/config/vars.mjs
+++ project/config/vars.mjs
@@ -1,6 +1,6 @@
 export const rootDir = process.cwd();
 export const projectDir = `${rootDir}/project`;
-export const tsconfigPath = `${rootDir}/config/tsconfig.json`;
-export const eslintConfigPath = `${rootDir}/config/eslint.config.js`;
+export const tsconfigPath = `${projectDir}/config/tsconfig.json`;
+export const eslintConfigPath = `${projectDir}/config/eslint.config.js`;
 
 console.log({ rootDir, projectDir, tsconfigPath, eslintConfigPath });
--- project/src/eslint.config.js
+++ project/src/eslint.config.js
@@ -1,3 +1,3 @@
-import config from '../../config/eslint.config.js';
+import config from '../config/eslint.config.js';
 
 export default config;
