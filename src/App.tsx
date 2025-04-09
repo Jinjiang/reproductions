8715069de8bc19f4e2f66630e0d351d4c96fe74f
Jinjiang <zhaojinjiang@me.com>
2025-04-09 11:19:17 +0800

    ssr usage - non-js file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { bar } from 'bar/bar-esm.mjs'
+import { bar } from 'bar/bar-tsx.tsx'
 
 export default function App() {
   return (<h1 className='bar'>{bar}</h1>)
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  ssr: {
-    // ensure all the imports of 'bar' being transformed
-    // before running in the server environment
-    noExternal: ['bar'],
-  },
   plugins: [react()],
 })
