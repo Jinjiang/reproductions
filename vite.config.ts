00f82493f07365536eedf217608108b4918dc610
Jinjiang <zhaojinjiang@me.com>
2025-04-09 11:40:44 +0800

    alias - fix ssr mode with dedupe

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
-    alias: [
-      {
-        find: 'foo',
-        replacement: `${__dirname}/node_modules/foo`,
-      },
-    ],
+    dedupe: ['foo'],
   },
   ssr: {
     noExternal: ['bar'],
