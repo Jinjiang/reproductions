f9e28dd2eee8ee39a8996e479dadeda894a99c1b
Jinjiang <zhaojinjiang@me.com>
2025-04-09 10:17:52 +0800

    optimizeDeps - equivalent exclude

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   optimizeDeps: {
-    noDiscovery: true,
-    include: [
-      `react-dom/client`,
-      'foo/foo-cjs.cjs',
-      // 'foo/foo-esm.mjs',
-      'foo/foo-cjs-module.cjs',
+    exclude: [
+      'foo/foo-esm.mjs',
     ],
   },
   plugins: [react()],
