a64ccfb5493c64afdf7fc0b8be0c297c4dfc109a
Jinjiang <zhaojinjiang@me.com>
2025-04-09 10:20:49 +0800

    optimizeDeps - fix with deep include

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   optimizeDeps: {
+    include: [
+      'foo > foo-dep-a/foo-dep-a-cjs.cjs',
+    ],
     exclude: [
       'foo/foo-esm.mjs',
     ],
