9ca7b43eb71d7703fd3bb8ff950e63ed86a040c2
Jinjiang <zhaojinjiang@me.com>
2025-04-09 11:24:38 +0800

    ssr.optimizeDeps - fix with ssr.noExternal + ssr.optimizeDeps

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  ssr: {
+    optimizeDeps: {
+      include: [
+        'baz/baz-cjs.cjs'
+      ],
+    },
+    noExternal: [
+      'baz'
+    ]
+  },
   plugins: [react()],
 })
