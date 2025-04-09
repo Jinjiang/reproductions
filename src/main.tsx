114cabd2f7cb16556b9237ed57ccf60347f2c523
Jinjiang <zhaojinjiang@me.com>
2025-04-09 10:11:30 +0800

    basic usage - module.exports in cjs

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,12 @@ import { createRoot } from 'react-dom/client'
 // 2 new imports
 import { foo as fooCjs } from 'foo/foo-cjs.cjs'
 import { foo as fooEsm } from 'foo/foo-esm.mjs'
+import fooCjsModule from 'foo/foo-cjs-module.cjs'
 
 import './index.css'
 import App from './App.tsx'
 
-console.log({ fooCjs, fooEsm });
+console.log({ fooCjs, fooEsm, fooCjsModule });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
