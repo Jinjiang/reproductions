039ae1169a9e74309f7c26747c56caa07b910748
Jinjiang <zhaojinjiang@me.com>
2025-04-09 10:50:24 +0800

    ssr usage - fix with ssr.noExternal

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  ssr: {
+    // ensure all the imports of 'bar' being transformed
+    // before running in the server environment
+    noExternal: ['bar'],
+  },
   plugins: [react()],
 })
