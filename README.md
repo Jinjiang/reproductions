77d1cb3b4e0c71c026bbd7fbe135a099ce175b17
Jinjiang <zhaojinjiang@me.com>
2025-05-07 17:51:53 +0800

    simplify the repro

--- README.md
+++ README.md
@@ -8,9 +8,7 @@ pnpm dev
 log:
 
 ```
-5:44:27 PM [vite] (client) ✨ new dependencies optimized: foo/foo1.mjs, foo/foo-a.cjs, foo/foo-b.cjs, foo/foo-c.mjs, @mdx-js/react, bar
-5:44:27 PM [vite] (client) ✨ optimized dependencies changed. reloading
-The file does not exist at "/<project-root>/node_modules/.vite/deps/chunk-E6KUT42S.js?v=05fb22f8" which is in the optimize deps directory. The dependency might be incompatible with the dep optimizer. Try adding it to `optimizeDeps.exclude`.
+5:50:59 PM [vite] (client) ✨ new dependencies optimized: foo/foo.mjs
 ```
 
 with Vite 5 there is no warnings:
--- debug/run.mjs
+++ debug/run.mjs
@@ -1,10 +1,5 @@
 import { writeFileSync } from 'fs';
 
-import react from "@vitejs/plugin-react";
-import mdx from "@mdx-js/rollup";
-
-import { mdxOptions } from './mdx.mjs';
-
 import { createServer as create5 } from 'vite5';
 import { createServer as create6 } from 'vite6';
 
@@ -12,18 +7,8 @@ const root = process.cwd();
 const port = 3000;
 
 const mainCode = `
-import foo from "${root}/node_modules/foo/foo1.mjs";
-import { foo as foo1 } from "${root}/node_modules/foo/foo-a.cjs";
-import { foo as foo2 } from "${root}/node_modules/foo/foo-b.cjs";
-import "${root}/node_modules/foo/foo-c.mjs";
-import docs from "${root}/debug/entries/docs.md";
-
-console.log({
-  foo,
-  foo1,
-  foo2,
-  docs,
-});
+import foo from "${root}/node_modules/foo/foo.mjs";
+console.log('foo', foo);
 `
 
 writeFileSync(`${root}/debug/entries/foo.mjs`, mainCode, { encoding: 'utf-8' });
@@ -41,10 +26,6 @@ const config = {
     ],
   },
   server: { port },
-  plugins: [
-    react(),
-    mdx(mdxOptions),
-  ],
 };
 
 console.log('vite version', process.env.vite || '6');
