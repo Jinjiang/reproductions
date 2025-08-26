6a9879cf4a35108e26a77d19845a90be9fa34a39
Jinjiang <zhaojinjiang@me.com>
2025-08-26 12:05:14 +0800

    fix

--- debug/run.mjs
+++ debug/run.mjs
@@ -10,7 +10,24 @@ const viteConfig = {
   root,
   configFile: false,
   envFile: false,
-  plugins: [vue()],
+  plugins: [
+    // walk around
+    // https://github.com/vitest-dev/vitest/issues/8468#issuecomment-3218289606
+    // {
+    //   name: "plugin-order-fixer",
+    //   configResolved(config) {
+    //     const coverageIndex = config.plugins.findIndex(
+    //       (plugin) => plugin.name === "vitest:coverage-transform"
+    //     );
+    //     const vueIndex = config.plugins.findIndex(
+    //       (plugin) => plugin.name === "vite:vue"
+    //     );
+    //     const [coveragePlugin] = config.plugins.splice(coverageIndex, 1);
+    //     config.plugins.splice(vueIndex + 1, 0, coveragePlugin);
+    //   },
+    // },
+    vue(),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
